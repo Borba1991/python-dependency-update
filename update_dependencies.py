1b065d9317ee7b491c2827c72e6a16a85a851b81
Bruno Guimarães Borba <162645879+Borba1991@users.noreply.github.com>
2024-10-09 21:26:59 -0300

    Update update_dependencies.py

--- update_dependencies.py
+++ update_dependencies.py
@@ -1,6 +1,6 @@
 import os
 def update_dependencies():
-os.system("pip install --upgrade -r requirements.txt")
-print("Dependências atualizadas com sucesso!")
+   os.system("pip install --upgrade -r requirements.txt")
+    print("Dependências atualizadas com sucesso!")
 if __name__ == "__main__":
-update_dependencies()
+  update_dependencies()
